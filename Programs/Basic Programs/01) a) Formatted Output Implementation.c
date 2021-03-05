f622abd415e4a7d6b59093e84a0e88a2a158e214
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-05 09:24:34 +0530

    Add files via upload

---
+++ Programs/Basic Programs/01) a) Formatted Output Implementation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{ 
+   printf("%4d|%-10s|%3d|%4d|%4d|%.2f",255,"Amit",9,81,150,50.00);
+   printf("\n%4d|%-10s|%3d|%4d|%4d|%.2f",2,"Sujata",70,80,200,66.678);  
+   printf("\n%4d|%-10s|%3d|%4d|%4d|%.2f",150,"Ajay",82,18,170,56.67); 
+   return 0;
+ }
+  

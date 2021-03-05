5835664ec0441fee78ce8e01007769fc0e0aa84a
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-05 10:43:30 +0530

    Add files via upload

---
+++ Programs/Basic Programs/07) Program To Find Ascii Value Of Given Character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  char ch;  
+    printf("Enter Character To Find Their Corresponding Ascii Value:");
+    scanf("%c",&ch);
+    clrscr();
+    printf("\nAscii Value Of Given Character .%c. Is %d",ch,ch);
+  return 0;
+ }
+  

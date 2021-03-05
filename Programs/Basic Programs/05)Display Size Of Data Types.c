3df2a89ca7de428e3ad4aa57022c0b2ba1320548
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-05 10:42:02 +0530

    Add files via upload

---
+++ Programs/Basic Programs/05)Display Size Of Data Types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+   printf("***Size Of Data Types***\n 1)Char=%d\n 2)Int=%d\n 3)Float=%d\n 4)Double=%d\n 5)Void=%d",sizeof(char),sizeof(int),sizeof(float),sizeof(double),sizeof(void));  
+  return 0;
+ }
+  

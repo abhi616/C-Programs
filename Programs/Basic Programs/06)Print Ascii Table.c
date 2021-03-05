958e22c67411a3ddf75eb42f8629925ece078db6
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-05 10:42:51 +0530

    Add files via upload

---
+++ Programs/Basic Programs/06)Print Ascii Table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  printf("****Ascii Table****\n");
+  for(int i=0;i<128;i++)
+   {
+     printf(" %d =\t%c \n",i,i);
+   }
+  return 0;
+ }
+  

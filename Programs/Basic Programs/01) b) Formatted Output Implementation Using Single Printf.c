be42ce4fcffa12798afde4a725914e2a705e2b39
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-05 09:26:18 +0530

    01) b) Formatted Output Implementation Using Single Printf.c

---
+++ Programs/Basic Programs/01) b) Formatted Output Implementation Using Single Printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{ 
+   printf("%4d|%-10s|%3d|%4d|%4d|%.2f\n%4d|%-10s|%3d|%4d|%4d|%.2f\n%4d|%-10s|%3d|%4d|%4d|%.2f",255,"Amit",9,81,150,50.00,2,"Sujata",70,80,200,66.678,150,"Ajay",82,18,170,56.67); 
+   return 0;
+ }
+  

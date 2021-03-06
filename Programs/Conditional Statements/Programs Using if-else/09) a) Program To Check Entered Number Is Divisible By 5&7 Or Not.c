42097514c4a020aaac1a244d0bcfad158397a1b8
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-06 12:34:45 +0530

    Add files via upload

---
+++ Programs/Conditional Statements/Programs Using if-else/09) a) Program To Check Entered Number Is Divisible By 5&7 Or Not.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  int no;
+    printf("Enter Number To Check It Is Divisible By 5&7 Or Not:");
+    scanf("%d",&no);    
+     if(no%5==0 && no%7==0)
+       {
+         printf("Entered Number %d Is Divisible By Both 5&7",no);
+       }
+     else if(no%5==0)
+       {
+         printf(" Entered Number %d Is Only Divisible By 5",no);
+       }
+    else if(no%7==0)
+      {
+        printf("Entered  Number %d Is Only Divisible By 7",no);
+      }
+    else
+        printf("Entered Number %d Is Not Divisible By Both 5&7",no);
+  return 0;
+ }
+  

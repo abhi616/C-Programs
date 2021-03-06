c980d67672e720001cd6bdb353c811624e4d82fc
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-06 12:34:09 +0530

    Add files via upload

---
+++ Programs/Conditional Statements/Programs Using if-else/08) a) Program To Check Entered Character Is Lowercase Or Uppercase Or Digit Or Special Symbol.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  char ch;
+    printf("Enter Character To Check Character Is Lowercase/Uppercase/Digit/Special Symbol:");
+    ch=getchar();    
+     if(ch>=65&&ch<=90)
+       {
+         printf("Entered Character '%c' Is Uppercase Character",ch);
+       }
+     else if(ch>=97&&ch<=122)
+       {
+         printf(" Entered Character '%c' Is Lowercase Character",ch);
+       }
+    else if(ch>=48&&ch<=57)
+      {
+        printf("Entered Character '%c' Is Digit",ch);
+      }
+    else
+        printf("Entered Character '%c' Is Special Symbol",ch);
+  return 0;
+ }
+  

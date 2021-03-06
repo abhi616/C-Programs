7fd00077a03dabd916f1a6c110239175ab300c2c
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-06 12:33:33 +0530

    Add files via upload

---
+++ Programs/Conditional Statements/Programs Using if-else/06) a) Program To Find Maximum Number From 3 Numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  int n1,n2,n3; 
+   up: 
+    printf("Enter Three Numbers To Check Which Number Is Maximum:");
+    scanf("%d%d%d",&n1,&n2,&n3);
+     if(n1==n2&&n1==n3)
+       {
+         printf("\nNumbers Are Equal...\n\n");
+         goto up;
+       }
+    if(n1>n2)
+      {
+      if(n1>n3)
+         {
+           printf("%d Is Maximum",n1);
+         }
+      else
+           printf("%d Is Maximum",n3);
+      }
+    else
+      {
+       if(n2>n3)
+         {
+           printf("%d Is Maximum",n2);
+          }
+       else
+          printf("%d Is Maximum",n3);   
+      }
+  return 0;
+ }
+  

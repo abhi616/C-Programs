fbe2f96cfc663a31b89937e0891cb5d602dd4447
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-06 12:37:24 +0530

    Add files via upload

---
+++ Programs/Conditional Statements/Programs Using Turnary Operator/1) b) Program To Find Maximum Number Between Two Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  int a,b;  
+    printf("Enter 2 Numbers To Find Max Number Between Them:");
+    scanf("%d%d",&a,&b);
+    (a>b?printf("\n%d is max",a):printf("\n%d is max",b));
+  return 0;
+ }
+  

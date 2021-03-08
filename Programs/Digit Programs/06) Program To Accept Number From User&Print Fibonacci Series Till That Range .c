03623ad13bcf0d3b77b7a5f510865972785546a1
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-08 14:13:08 +0530

    Add files via upload

---
+++ Programs/Digit Programs/06) Program To Accept Number From User&Print Fibonacci Series Till That Range .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  int n,f=0,s=1,fib;
+    printf("Enter Number To Print Fibonacci Series Till That Range:");
+    scanf("%d",&n); 
+     for(int i=0;i<=n;i++)
+      {
+         fib=f;
+         printf("%d ",fib);
+           f=s; 
+           s=fib+f;         
+      }
+  return 0;
+ }
+  

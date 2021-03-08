68a4362f905b7e4128647b410e78b0892d598b86
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-08 09:09:54 +0530

    Add files via upload

---
+++ Programs/Digit Programs/01) Program To Find Sum Of Entered 3 Digit Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+int main()
+{  
+  int n,sum=0,temp,dig=0;
+     up:
+    printf("Enter 3 Digit Number To Find Their Sum:");
+    scanf("%d",&n); 
+     temp=n; 
+   if(temp>=100 && temp<=999) 
+     {
+     for(;temp>0;temp=temp/10)
+      {
+         dig=temp%10;
+         sum=sum+dig;
+      }
+   }
+   else
+     {
+      printf("\nEnter Valid 3 Digit Number....\n\n");
+      goto up; 
+     }
+     printf("\nSum Of Entered Number %d Is %d",n,sum);
+  return 0;
+ }
+  

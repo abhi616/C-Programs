039379e7277721eef643a556d7409fa9eb4c79c4
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-08 14:10:05 +0530

    Add files via upload

---
+++ Programs/Digit Programs/05) Program To Accept 2 Numbers & Print Their Factors.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+  int n1,n2;  
+    printf("Enter 2 Numbers:"); 
+    scanf("%d%d",&n1,&n2); 
+    printf("\nFactors Of %d Is==>\n",n1);
+    for(int i=1;i<=n1;i++)
+     {
+       if(n1%i==0)
+         {
+          printf("\t\t%d \n",i);
+         }     
+     }
+    printf("\nFactors Of %d Is==>\n",n2);
+    for(int i=1;i<=n2;i++)
+     {
+       if(n2%i==0)
+         {
+          printf("\t\t%d \n",i);
+         }     
+     }
+    
+  return 0;
+}
+
+    

096b1a37c2d485f2d6291f559c4c68cff4afa87c
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-10 22:23:11 +0530

    Add files via upload

---
+++ Programs/Array Programs Assignment/03)Create Array Of 7 Elements With Null Initialisation & Display All Elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int Arr[7]={0},i;
+  
+  for(i=0;i<7;i++)
+   {
+     printf("%d Element = %d\n",i+1,Arr[i]);     
+   }
+  
+  return 0;
+}

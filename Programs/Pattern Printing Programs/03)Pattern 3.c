f032c20d0713e50c7970b0649448a6aaa568682e
Abhi Yadav <ay891241@gmail.com>
2021-05-15 15:13:50 +0530

    Add files via upload

---
+++ Programs/Pattern Printing Programs/03)Pattern 3.c
@@ -0,0 +1,43 @@
+/*
+     Input  :   R=5  C=5
+     Output :
+              * * * * *
+              *       *
+              *       *
+              *       *
+              * * * * *             
+*/
+  
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int R,C,i,j;
+  
+  printf("Enter Row & Column Count : ");
+  scanf("%d%d",&R,&C);
+  
+  for(i=1;i<=R;i++)
+  {
+     for(j=1;j<=C;j++)
+      {
+        if(i==1||i==R||j==1||j==C)
+        {
+          printf("* ");
+        }       
+       else
+        {         
+          printf("  ");
+        }
+      }
+      
+      printf("\n");
+  }
+  
+  getch();
+  return 0;
+}

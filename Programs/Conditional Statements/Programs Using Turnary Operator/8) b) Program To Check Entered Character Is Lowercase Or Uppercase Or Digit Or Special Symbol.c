56fb79ab4c8e6bf8124e721028cb957f8b6071fc
Abhi Yadav <ay891241@gmail.com>
2021-05-29 22:41:07 +0530

    Update 8) b) Program To Check Entered Character Is Lowercase Or Uppercase Or Digit Or Special Symbol.c

--- Programs/Conditional Statements/Programs Using Turnary Operator/8) b) Program To Check Entered Character Is Lowercase Or Uppercase Or Digit Or Special Symbol.c
+++ Programs/Conditional Statements/Programs Using Turnary Operator/8) b) Program To Check Entered Character Is Lowercase Or Uppercase Or Digit Or Special Symbol.c
@@ -4,10 +4,14 @@
 //Compiler version gcc  6.3.0
 int main()
 {  
-  char ch;
+    char ch;
+
     printf("Enter Character To Check Character Is Lowercase/Uppercase/Digit/Special Symbol:");
-    ch=getchar();    
-     ((ch>=65&&ch<=90)?printf("\nEntered Character '%c' Is Uppercase Character",ch):(ch>=97&&ch<=122)?printf("\nEntered Character '%c' Is Lowercase Character",ch):(ch>=48&&ch<=57)?printf("\nEntered Character '%c' Is Digit",ch):printf("\nEntered Character '%c' Is Special Symbol",ch));      
-  return 0;
+    ch=getchar();  
+  
+   ((ch>=65&&ch<=90)?printf("\nEntered Character '%c' Is Uppercase Character",ch):(ch>=97&&ch<=122)?printf("\nEntered Character '%c' Is Lowercase Character",ch):(ch>=48&&ch<=57)?printf("\nEntered Character '%c' Is Digit",ch):printf("\nEntered Character '%c' Is Special Symbol",ch));      
+   
+   getch();
+   return 0;
  }
   

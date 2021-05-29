288303697b62ee4c09949a94e2f4c361c4237788
Abhi Yadav <ay891241@gmail.com>
2021-05-29 22:36:37 +0530

    Update 3) b) Program To Check Given Number Is Even Or Odd.c

--- Programs/Conditional Statements/Programs Using Turnary Operator/3) b) Program To Check Given Number Is Even Or Odd.c
+++ Programs/Conditional Statements/Programs Using Turnary Operator/3) b) Program To Check Given Number Is Even Or Odd.c
@@ -2,12 +2,17 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no;  
+    int No;  
+
     printf("Enter Number To Check Number Is Even Or Odd:");
-    scanf("%d",&no);
-     (no%2==0?printf("%d Is Even Number",no):printf("%d Is Odd Number",no));              
-  return 0;
+    scanf("%d",&No);
+
+    (No%2==0?printf("%d Is Even Number",No):printf("%d Is Odd Number",No));  
+
+    getch();            
+    return 0;
  }
   

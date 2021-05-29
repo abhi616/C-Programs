29cf2410f8e67b26782477827a32d8a50a7ad9a0
Abhi Yadav <ay891241@gmail.com>
2021-05-29 22:35:27 +0530

    Update 2) b) Program To Check Given Number Is Prime Or Not.c

--- Programs/Conditional Statements/Programs Using Turnary Operator/2) b) Program To Check Given Number Is Prime Or Not.c
+++ Programs/Conditional Statements/Programs Using Turnary Operator/2) b) Program To Check Given Number Is Prime Or Not.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no,p=1,i,temp;  
+  int No,p=1,i,temp;  
+
     up:
     printf("Enter Number To Check Number Is Prime Or Not:");
-    scanf("%d",&no);
-      temp=no;
+    scanf("%d",&No);
+
+      temp=No;
+
       if(temp==0||temp==1)
       {
         printf("\nNeutral Number\n");
-        goto up;     //Goes Control to 'up' variable  
+        goto up;                //Goes Control to 'up' variable  
       }
+
      for(i=2;i<=temp/2;i++)
       {
         if(temp%i==0)
@@ -22,8 +27,10 @@ int main()
             break;
           }        
       }
- (p==1?printf("\n%d Is Prime",no):printf("\n%d Is Not Prime",no));
-        
+
+ (p==1?printf("\n%d Is Prime",No):printf("\n%d Is Not Prime",No));
+     
+  getch();   
   return 0;
  }
   

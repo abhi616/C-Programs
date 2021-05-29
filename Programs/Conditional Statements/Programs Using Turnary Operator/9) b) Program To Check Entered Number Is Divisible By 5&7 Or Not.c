a2e7d4da043f9b501e6770349235501c3d52b71d
Abhi Yadav <ay891241@gmail.com>
2021-05-29 22:42:59 +0530

    Update 9) b) Program To Check Entered Number Is Divisible By 5&7 Or Not.c

--- Programs/Conditional Statements/Programs Using Turnary Operator/9) b) Program To Check Entered Number Is Divisible By 5&7 Or Not.c
+++ Programs/Conditional Statements/Programs Using Turnary Operator/9) b) Program To Check Entered Number Is Divisible By 5&7 Or Not.c
@@ -2,12 +2,17 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no;
+    int No;
+
     printf("Enter Number To Check It Is Divisible By 5&7 Or Not:");
-    scanf("%d",&no);    
-   ((no%5==0 && no%7==0)?printf("Entered Number %d Is Divisible By Both 5&7",no):(no%5==0)?printf(" Entered Number %d Is Only Divisible By 5",no):(no%7==0)?printf("Entered  Number %d Is Only Divisible By 7",no):printf("Entered Number %d Is Not Divisible By Both 5&7",no));
-  return 0;
+    scanf("%d",&No);  
+  
+   ((No%5==0 && No%7==0)?printf("Entered Number %d Is Divisible By Both 5&7",No):(No%5==0)?printf(" Entered Number %d Is Only Divisible By 5",No):(No%7==0)?printf("Entered  Number %d Is Only Divisible By 7",No):printf("Entered Number %d Is Not Divisible By Both 5&7",No));
+ 
+   getch();
+   return 0;
  }
   

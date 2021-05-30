cb3348ac91f6e27d10d1d9fa9549bd33fc90af4b
Abhi Yadav <ay891241@gmail.com>
2021-05-30 21:16:14 +0530

    Update 01) a) Program To Find Maximum Number Between Two Numbers.c

--- Programs/Conditional Statements/Programs Using if-else/01) a) Program To Find Maximum Number Between Two Numbers.c
+++ Programs/Conditional Statements/Programs Using if-else/01) a) Program To Find Maximum Number Between Two Numbers.c
@@ -2,17 +2,22 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int a,b;  
-    printf("Enter 2 Numbers To Find Max Number Between Them:");
+    int a,b;  
+
+    printf("Enter 2 Numbers To Find Max Number Between Them : ");
     scanf("%d%d",&a,&b);
+
      if(a>b)
       {
        printf("%d is maximum",a);
       }
      else
       printf("%d is maximum",b);
-  return 0;
+
+   getch();
+   return 0;
  }
   

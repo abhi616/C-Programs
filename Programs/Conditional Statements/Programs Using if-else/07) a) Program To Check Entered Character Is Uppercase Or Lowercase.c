fa53cf34afb0fcecd5e430e7874631d51577d1d0
Abhi Yadav <ay891241@gmail.com>
2021-05-30 21:23:01 +0530

    Update 07) a) Program To Check Entered Character Is Uppercase Or Lowercase.c

--- Programs/Conditional Statements/Programs Using if-else/07) a) Program To Check Entered Character Is Uppercase Or Lowercase.c
+++ Programs/Conditional Statements/Programs Using if-else/07) a) Program To Check Entered Character Is Uppercase Or Lowercase.c
@@ -2,17 +2,24 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
   char ch;
+
     printf("Enter Character To Check Character Is Lowercase Or Uppercase:");
-     ch=getchar();    
+    ch=getchar();   
+ 
      if(ch>=65&&ch<=90)
       {
         printf("\n%c Is Uppercase",ch);
       }
-   else
-       printf("\n%c Is Lowercase",ch);
+    else
+      {
+        printf("\n%c Is Lowercase",ch);
+      }
+
+  getch();
   return 0;
  }
   

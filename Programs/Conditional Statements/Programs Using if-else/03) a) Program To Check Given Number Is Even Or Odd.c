69929b7c1b360e4d774c5338f53d344836a170df
Abhi Yadav <ay891241@gmail.com>
2021-05-30 21:19:09 +0530

    Update 03) a) Program To Check Given Number Is Even Or Odd.c

--- Programs/Conditional Statements/Programs Using if-else/03) a) Program To Check Given Number Is Even Or Odd.c
+++ Programs/Conditional Statements/Programs Using if-else/03) a) Program To Check Given Number Is Even Or Odd.c
@@ -2,17 +2,22 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no;  
+    int no;  
+
     printf("Enter Number To Check Number Is Even Or Odd:");
     scanf("%d",&no);
+
      if(no%2==0)
       {
         printf("%d Is Even Number",no);
       }
      else
-        printf("%d Is Odd Number",no);       
+        printf("%d Is Odd Number",no);  
+
+  getch();     
   return 0;
  }
   

7e5f5910eb65bbcefce57279bd44119ad0e2cc65
Abhi Yadav <ay891241@gmail.com>
2021-05-30 21:32:48 +0530

    Update 10) Program To Find Maximum Digit From The Entered 3 Digit Number.c

--- Programs/Conditional Statements/Programs Using if-else/10) Program To Find Maximum Digit From The Entered 3 Digit Number.c
+++ Programs/Conditional Statements/Programs Using if-else/10) Program To Find Maximum Digit From The Entered 3 Digit Number.c
@@ -2,15 +2,19 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
   int n,max=0,temp,dig=0;
-     up:
+
+    up:
     printf("Enter 3 Digit Number To Find Maximum Digit From It:");
     scanf("%d",&n); 
-     temp=n; 
+
+   temp=n; 
+
    if(temp>=100 && temp<=999) 
-     {
+    {
      for(;temp>0;temp=temp/10)
       {
          dig=temp%10;
@@ -19,13 +23,16 @@ int main()
               max=dig;
             }       
       }
-   }
+    }
    else
      {
-      printf("\nEnter Valid 3 Digit Number....\n\n");
-      goto up; 
+       printf("\nEnter Valid 3 Digit Number....\n\n");
+       goto up; 
      }
+
      printf("\nMaximum Digit From The Entered Number %d Is %d",n,max);
+  
+  getch();
   return 0;
  }
   

4e0d879db3d9e46325c668c98bdb26d94dea2873
Abhi Yadav <ay891241@gmail.com>
2021-05-30 21:18:22 +0530

    Update 02) a) Program To Check Given Number Is Prime Or Not.c

--- Programs/Conditional Statements/Programs Using if-else/02) a) Program To Check Given Number Is Prime Or Not.c
+++ Programs/Conditional Statements/Programs Using if-else/02) a) Program To Check Given Number Is Prime Or Not.c
@@ -2,18 +2,23 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no,p=1,i,temp;  
+  int No,p=1,i,temp;  
+
     up:
-    printf("Enter Number To Check Number Is Prime Or Not:");
-    scanf("%d",&no);
-      temp=no;
+    printf("Enter Number To Check Number Is Prime Or Not: ");
+    scanf("%d",&No);
+
+      temp=No;
+
     if(temp==0||temp==1)
       {
         printf("\nNeutral Number\n");
-        goto up;     //Goes Control to 'up' variable  
+        goto up;             //Goes Control to 'up' variable  
       }
+
      for(i=2;i<=temp/2;i++)
       {
         if(temp%i==0)
@@ -22,12 +27,15 @@ int main()
              break;
           }
       }
+
      if(p==1)
        {
-          printf("\n%d Is Prime",no);
+          printf("\n%d Is Prime",No);
         }
      else 
-        printf("\n%d Is Not Prime",no);
+        printf("\n%d Is Not Prime",No);
+
+  getch();
   return 0;
  }
   

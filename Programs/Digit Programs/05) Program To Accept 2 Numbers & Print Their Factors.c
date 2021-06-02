a41c837d6f1a1a01baa3766d6d4a82e125e7e3fb
Abhi Yadav <ay891241@gmail.com>
2021-06-02 22:39:39 +0530

    Update 05) Program To Accept 2 Numbers & Print Their Factors.c

--- Programs/Digit Programs/05) Program To Accept 2 Numbers & Print Their Factors.c
+++ Programs/Digit Programs/05) Program To Accept 2 Numbers & Print Their Factors.c
@@ -3,10 +3,13 @@
 
 int main()
 { 
-  int n1,n2;  
+    int n1,n2;  
+
     printf("Enter 2 Numbers:"); 
     scanf("%d%d",&n1,&n2); 
+
     printf("\nFactors Of %d Is==>\n",n1);
+
     for(int i=1;i<=n1;i++)
      {
        if(n1%i==0)
@@ -14,7 +17,9 @@ int main()
           printf("\t\t%d \n",i);
          }     
      }
+
     printf("\nFactors Of %d Is==>\n",n2);
+
     for(int i=1;i<=n2;i++)
      {
        if(n2%i==0)
@@ -22,7 +27,8 @@ int main()
           printf("\t\t%d \n",i);
          }     
      }
-    
+   
+  getch(); 
   return 0;
 }
 

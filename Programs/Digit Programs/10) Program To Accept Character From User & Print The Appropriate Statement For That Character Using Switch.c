f8783fbb972d399447424013e10a0b2b808109fc
Abhi Yadav <ay891241@gmail.com>
2021-06-02 22:43:54 +0530

    Update 10) Program To Accept Character From User & Print The Appropriate Statement For That Character Using Switch.c

--- Programs/Digit Programs/10) Program To Accept Character From User & Print The Appropriate Statement For That Character Using Switch.c
+++ Programs/Digit Programs/10) Program To Accept Character From User & Print The Appropriate Statement For That Character Using Switch.c
@@ -2,11 +2,13 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
   char ch;
     printf("Enter Character Between[(A-D)/(a-d)]:");
     scanf("%c",&ch); 
+
      switch(ch)
      {
        case 'A':
@@ -28,6 +30,8 @@ int main()
      default:
               printf("Thanks...");
     }
+
+  getch();
   return 0;
  }
   

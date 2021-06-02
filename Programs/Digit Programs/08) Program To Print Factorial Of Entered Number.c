9f93d98ec87a308ea1c6c21422b90e8575f5655b
Abhi Yadav <ay891241@gmail.com>
2021-06-02 22:43:13 +0530

    Update 08) Program To Print Factorial Of Entered Number.c

--- Programs/Digit Programs/08) Program To Print Factorial Of Entered Number.c
+++ Programs/Digit Programs/08) Program To Print Factorial Of Entered Number.c
@@ -2,12 +2,16 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int n,fact=1,temp;
+    int n,fact=1,temp;
+
     printf("Enter Number To Find Their Factorial:");
     scanf("%d",&n); 
-      temp=n;
+
+    temp=n;
+
     if(temp>0)
     {
       for(;temp>0;temp--)
@@ -22,7 +26,10 @@ int main()
         fact=temp*fact;      
       }
     }
-    printf("\nFactorial Of Entered Number %d Is %d",n,fact);
+
+  printf("\nFactorial Of Entered Number %d Is %d",n,fact);
+  
+  getch();
   return 0;
  }
   

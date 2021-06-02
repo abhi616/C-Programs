65b5fea9d15b0482d15b0dde8b05e81f2ca6b620
Abhi Yadav <ay891241@gmail.com>
2021-06-02 22:42:24 +0530

    Update 07) Program To Check Entered Number Is Palindrome Or Not.c

--- Programs/Digit Programs/07) Program To Check Entered Number Is Palindrome Or Not.c
+++ Programs/Digit Programs/07) Program To Check Entered Number Is Palindrome Or Not.c
@@ -2,23 +2,32 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int n,rev=0,temp,dig=0;
+    int n,rev=0,temp,dig=0;
+
     printf("Enter Number To Check It Is Palindrome Or Not:");
     scanf("%d",&n); 
-     temp=n;  
-     for(;temp>0;temp=temp/10)
-      {
-         dig=temp%10;
-         rev=rev*10+dig;
-      }
+
+    temp=n;  
+
+   for(;temp>0;temp=temp/10)
+    {
+       dig=temp%10;
+       rev=rev*10+dig;
+    }
+
     if(rev==n)
      {
        printf("\n %d Is Palindrome...",n);
      }
     else
+     {
        printf("\n %d Is Not Palindrome...",n);
+     }
+
+  getch();
   return 0;
  }
   

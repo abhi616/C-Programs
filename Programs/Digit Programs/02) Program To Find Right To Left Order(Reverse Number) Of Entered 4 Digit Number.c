6ded0061255802bdf008b81acdf091e2d627d57d
Abhi Yadav <ay891241@gmail.com>
2021-06-02 22:35:38 +0530

    Update 02) Program To Find Right To Left Order(Reverse Number) Of Entered 4 Digit Number.c

--- Programs/Digit Programs/02) Program To Find Right To Left Order(Reverse Number) Of Entered 4 Digit Number.c
+++ Programs/Digit Programs/02) Program To Find Right To Left Order(Reverse Number) Of Entered 4 Digit Number.c
@@ -2,27 +2,34 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int n,rev=0,temp,dig=0;
-     up:
+   int n,rev=0,temp,dig=0;
+    
+    up:
     printf("Enter 4 Digit Number To Find Their Right To Left Order(Reverse Number):");
     scanf("%d",&n); 
-     temp=n; 
-   if(temp>=1000&& temp<=9999) 
-     {
+
+   temp=n; 
+
+  if(temp>=1000&& temp<=9999) 
+   {
      for(;temp>0;temp=temp/10)
       {
          dig=temp%10;
          rev=rev*10+dig;
       }
    }
-   else
-     {
+  else
+   {
       printf("\nEnter Valid 4 Digit Number....\n\n");
       goto up; 
-     }
-     printf("\nEntered Number %d Right To Left Order Is %d",n,rev);
+   }
+
+   printf("\nEntered Number %d Right To Left Order Is %d",n,rev);
+  
+  getch();
   return 0;
  }
   

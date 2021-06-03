89cb7c29137dbea642242985898318ec7ddb426a
Abhi Yadav <ay891241@gmail.com>
2021-06-03 23:56:09 +0530

    Update 02)Accept Student Information & Display All Info Using Single Printf.c

--- Programs/Basic Programs/02)Accept Student Information & Display All Info Using Single Printf.c
+++ Programs/Basic Programs/02)Accept Student Information & Display All Info Using Single Printf.c
@@ -2,23 +2,31 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 { 
   char nm[20],course[10],city[10];
   int rno;
+
    printf("Enter Your Name:"); 
    gets(nm);  
+
    printf("\nEnter Your Roll Number:");
    scanf("%d",&rno);
+
    printf("\nEnter Your Course Name:");
    scanf("%s",&course);
+
    printf("\nEnter Your City Name:");
    scanf("%s",&city);
+
    clrscr();
-     printf("*****Student Information*****");
+
+   printf("*****Student Information*****");
  
-    printf("\nName==>%s\nRoll No.==>%d\nCourse Name==>%s\nCity Name==>%s\n",nm,rno,course,city);
+   printf("\nName==>%s\nRoll No.==>%d\nCourse Name==>%s\nCity Name==>%s\n",nm,rno,course,city);
     
-return 0;
+  getch();
+  return 0;
  }
   

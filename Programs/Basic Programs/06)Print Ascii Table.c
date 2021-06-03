eee52bdbcbb26165f3d14ccc3b7e4514b3d1de2a
Abhi Yadav <ay891241@gmail.com>
2021-06-04 00:03:21 +0530

    Update 06)Print Ascii Table.c

--- Programs/Basic Programs/06)Print Ascii Table.c
+++ Programs/Basic Programs/06)Print Ascii Table.c
@@ -2,13 +2,17 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
   printf("****Ascii Table****\n");
+
   for(int i=0;i<128;i++)
    {
      printf(" %d =\t%c \n",i,i);
    }
+
+  getch();
   return 0;
  }
   

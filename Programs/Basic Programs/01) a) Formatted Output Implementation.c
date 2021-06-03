8903f0c5fa6dc2471e46a3b5cdedf6be9cdb1f2b
Abhi Yadav <ay891241@gmail.com>
2021-06-03 23:54:26 +0530

    Update 01) a) Formatted Output Implementation.c

--- Programs/Basic Programs/01) a) Formatted Output Implementation.c
+++ Programs/Basic Programs/01) a) Formatted Output Implementation.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
@@ -7,6 +7,8 @@ int main()
    printf("%4d|%-10s|%3d|%4d|%4d|%.2f",255,"Amit",9,81,150,50.00);
    printf("\n%4d|%-10s|%3d|%4d|%4d|%.2f",2,"Sujata",70,80,200,66.678);  
    printf("\n%4d|%-10s|%3d|%4d|%4d|%.2f",150,"Ajay",82,18,170,56.67); 
-   return 0;
+   
+  getch();
+  return 0;
  }
   

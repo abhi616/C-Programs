e4a28ea0d1e701e5bc122ad2991eb200733019f0
Abhi Yadav <ay891241@gmail.com>
2021-06-04 00:04:45 +0530

    Update 08) Program To Find Character For Given Ascii Value.c

--- Programs/Basic Programs/08) Program To Find Character For Given Ascii Value.c
+++ Programs/Basic Programs/08) Program To Find Character For Given Ascii Value.c
@@ -2,13 +2,19 @@
 #include<conio.h>
 
 //Compiler version gcc  6.3.0
+
 int main()
 {  
-  int no;  
+    int no;  
+
     printf("Enter Number To Find Their Corresponding Ascii Character:");
     scanf("%d",&no);
+
     clrscr();
+
     printf("\nCharacter Of Given Ascii Value %d Is %c",no,no);
-  return 0;
+    
+    getch();
+    return 0;
  }
   

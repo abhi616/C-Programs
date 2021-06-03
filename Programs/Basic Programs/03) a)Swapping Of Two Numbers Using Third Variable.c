a5293d336c28aa8f58b0fbfa2bce4b95be8756e1
Abhi Yadav <ay891241@gmail.com>
2021-06-03 23:57:27 +0530

    Update 03) a)Swapping Of Two Numbers Using Third Variable.c

--- Programs/Basic Programs/03) a)Swapping Of Two Numbers Using Third Variable.c
+++ Programs/Basic Programs/03) a)Swapping Of Two Numbers Using Third Variable.c
@@ -5,15 +5,21 @@
 int main()
 { 
   int no1,no2,temp; 
+
    printf("Enter Two Numbers For Swapping:");
    scanf("%d%d",&no1,&no2);
+
    clrscr();
-  printf("Before Swapping No1=%d & No2=%d",no1,no2);
+
+   printf("Before Swapping No1=%d & No2=%d",no1,no2);
+
      temp=no1;
       no1=no2;
       no2=temp;
-  printf("\n\nAfter Swapping No1=%d & No2=%d\n",no1,no2);
 
+   printf("\n\nAfter Swapping No1=%d & No2=%d\n",no1,no2);
+
+  getch();
   return 0;
  }
   

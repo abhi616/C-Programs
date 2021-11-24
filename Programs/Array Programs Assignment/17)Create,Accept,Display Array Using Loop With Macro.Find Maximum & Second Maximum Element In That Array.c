69eab78848209b9d60d1e5d6611af29851fe35ac
Abhi Yadav <ay891241@gmail.com>
2021-11-24 20:38:18 +0530

    Add files via upload

--- Programs/Array Programs Assignment/17)Create,Accept,Display Array Using Loop With Macro.Find Maximum & Second Maximum Element In That Array.c
+++ Programs/Array Programs Assignment/17)Create,Accept,Display Array Using Loop With Macro.Find Maximum & Second Maximum Element In That Array.c
@@ -6,57 +6,59 @@
 
 void Find_FirstMaximum_SecondMaximum(int *P,int *Max,int *S_Max)
 {
-  int j=0;
-  
-  for(j=0;j<Cnt;j++) 
-      {
-        if(j==0)
-         {
-           *Max=P[j];
-         }
-         
-         if(P[j]>*Max)
+      int j=0;
+
+      for(j=0;j<Cnt;j++) 
           {
-            *S_Max=*Max;
-            *Max=P[j];            
+            if(j==0)
+             {
+               *Max=P[j];
+             }
+
+             if(P[j]>*Max)
+              {
+                *S_Max=*Max;
+                *Max=P[j];            
+              }
+
+
           }
-         
-        
-      }
-      
-   return;
+
+       return;
 }
 
 int main()
 {
-  int Arr[Cnt]={0},i=0,Maxi=0,S_Maxi=0;
+      int Arr[Cnt]={0},i=0,Maxi=0,S_Maxi=0;
+
+        //Accept Array Elements Using Loop
+
+         for(i=0;i<Cnt;i++)
+          {
+            printf("\n•Enter %d Element: ",i+1);
+            scanf("%d",&Arr[i]);
+
+          }
+
+
+         printf("\n\n*************************\n");
+
+         //Display Array Elements Using Loop
+
+         for(i=0;i<Cnt;i++)
+          {
+            printf("\n%d Element = %d\n",i+1,Arr[i]);
+          }
+
+
+         printf("\n\n*************************\n");
+
+
+        Find_FirstMaximum_SecondMaximum(Arr,&Maxi,&S_Maxi);
+
+         printf("\n1st Maximum Number From Given Array = %d\n",Maxi);
+         printf("\n2nd Maximum Number From Given Array = %d\n",S_Maxi);
   
-    //Accept Array Elements Using Loop
-     
-     for(i=0;i<Cnt;i++)
-      {
-        printf("\n•Enter %d Element: ",i+1);
-        scanf("%d",&Arr[i]);
-     
-      }
-     
-     
-     printf("\n\n*************************\n");
-     
-     //Display Array Elements Using Loop
-     
-     for(i=0;i<Cnt;i++)
-      {
-        printf("\n%d Element = %d\n",i+1,Arr[i]);
-      }
-     
-      
-     printf("\n\n*************************\n");
-     
-     
-    Find_FirstMaximum_SecondMaximum(Arr,&Maxi,&S_Maxi);
-     
-     printf("\n1st Maximum Number From Given Array = %d\n",Maxi);
-     printf("\n2nd Maximum Number From Given Array = %d\n",S_Maxi);
-  return 0;
+        getch();
+        return 0;
 }

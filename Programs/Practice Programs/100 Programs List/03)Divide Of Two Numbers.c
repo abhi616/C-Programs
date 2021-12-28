c5bf91172860ba2ace9ffa960152e0c6b1c06f24
Abhi Yadav <ay891241@gmail.com>
2021-12-28 22:06:18 +0530

    Add files via upload

---
+++ Programs/Practice Programs/100 Programs List/03)Divide Of Two Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+     int iN1,iN2,Div=0;
+
+     printf("\n Enter Two Numbers : ");
+     scanf("%d%d",&iN1,&iN2);
+
+     Div = iN1/iN2;
+
+     printf("\n Division = %d\n",Div);
+
+     getch();
+     return 0;
+
+}
+

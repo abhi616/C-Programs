cb832d07c86de3b7cdd6cd42ae235f4e0c29a7fa
Abhi Yadav <ay891241@gmail.com>
2021-12-28 21:43:33 +0530

    Add files via upload

---
+++ Programs/Practice Programs/100 Programs List/01)Find Sum Of Two Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+     int iN1,iN2,Sum=0;
+
+     printf("\n Enter Two Numbers : ");
+     scanf("%d%d",&iN1,&iN2);
+
+
+     Sum = iN1 + iN2;
+
+     printf("\n Sum = %d\n",Sum);
+
+     getch();
+     return 0;
+
+}

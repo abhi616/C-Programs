c4ab3778b29508c8a24e3734ef8dc356d84700a2
Abhi Yadav <ay891241@gmail.com>
2021-12-28 21:44:35 +0530

    Add files via upload

---
+++ Programs/Practice Programs/100 Programs List/02)Find Difference Of Two Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+     int iN1,iN2,Diff=0;
+
+     printf("\n Enter Two Numbers : ");
+     scanf("%d%d",&iN1,&iN2);
+
+     Diff = iN1 - iN2;
+
+     printf("\n Difference = %d\n",Diff);
+
+     getch();
+     return 0;
+
+}
+

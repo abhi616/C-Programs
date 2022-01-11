11a12577780148d775b0c2796119d05ecd0f3eae
Abhi Yadav <ay891241@gmail.com>
2022-01-11 14:10:04 +0530

    Add files via upload

---
+++ Programs/String Programs/Remove Character From String.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+      char Str[30],Ch,Str2[30];
+      int Cnt=0;
+
+      printf("\n Enter String : ");
+      scanf("%[^\n]",&Str);
+
+      fflush(stdin);
+
+      printf("\n Which Charcter You Want To Remove : ");
+      scanf("%c",&Ch);
+
+      printf("\n------------------------------------------------------------------\n");
+
+      for(int i=0,j=0;Str[i]!='\0';i++)
+      {
+          if(Str[i]==Ch)
+          {
+            Cnt++;
+            continue;
+          }
+          Str2[j] = Str[i];
+          j++;
+      }
+
+      if(Cnt>0)
+      {
+          printf("\n After Removing '%c' From Entered String.\n\n Final String = %s\n",Ch,Str2);
+      }
+     else
+      {
+         printf("\n Sorry But Entered String Doesn't Contain '%c' In It.\n\n Final String = %s\n",Ch,Str2);
+      }
+
+
+      printf("\n------------------------------------------------------------------\n");
+
+      getch();
+      return 0;
+
+}

95a4db89c1369cb7851443920e481a1c23362868
Abhi Yadav <ay891241@gmail.com>
2022-01-21 22:18:57 +0530

    Add files via upload

---
+++ Programs/Pattern Printing Programs/Assignment/Pattern 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+      int i,j;
+      char ch='A';
+
+         for(i=1;i<=5;i++)
+         {
+          for(j=1;i>=j;j++)
+            {
+                printf("%c ",ch++);
+            }
+           printf("\n");
+         }
+
+      return 0;
+ }

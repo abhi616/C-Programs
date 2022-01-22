ef8f708f40868d4b020fc8b53dd0ea605ca0f3f9
Abhi Yadav <ay891241@gmail.com>
2022-01-22 22:02:12 +0530

    Add files via upload

---
+++ Programs/Pattern Printing Programs/Assignment/Pattern 11.c
@@ -0,0 +1,37 @@
+/*
+
+    Input : Enter character To Print pattern : A
+
+    Output :
+             A
+             A  A
+             A  A  A
+             A  A  A  A
+             A  A  A  A  A
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+      int i,j;
+      char ch;
+
+        printf("\n Enter character To Print pattern: ");
+        scanf("%c",&ch);
+
+        for(i=1;i<=5;i++)
+         {
+           for(j=1;i>=j;j++)
+            {
+                printf(" %c ",ch);
+            }
+           printf("\n");
+         }
+
+      getch();
+      return 0;
+ }

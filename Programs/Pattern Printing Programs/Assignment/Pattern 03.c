b15795712d32ab7481fbae58f41ad8edae1b6ed7
Abhi Yadav <ay891241@gmail.com>
2022-01-23 21:58:01 +0530

    Pattern_03

---
+++ Programs/Pattern Printing Programs/Assignment/Pattern 03.c
@@ -0,0 +1,33 @@
+/*
+
+    Output :
+             A
+             A  B
+             A  B  C
+             A  B  C  D
+             A  B  C  D  E
+
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+      int i,j;
+      char ch='A';
+
+         for(i=1;i<=5;i++)
+         {
+          for(j=1,ch='A';i>=j;j++,ch++)
+            {
+               printf(" %c ",ch);
+            }
+           printf("\n");
+         }
+
+
+      getch();
+      return 0;
+ }
